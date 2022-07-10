b60fdb4e3b4382ed19f83bcba846df6eefd9b040
Valenwe <louis.vie@efrei.net>
2022-07-10 20:48:28 +0200

    Update db.php

--- Server/BACKEND/db.php
+++ Server/BACKEND/db.php
@@ -4,6 +4,5 @@
 // root -> no password
 // mysql.exe -u root -p
 $db = mysqli_connect('localhost', 'root', '', 'filelocker');
-// $db = mysqli_connect('localhost', 'filelocker', 'oX8/p7Isd@', 'filelocker');
 
 if (!$db) die("Connection failed: " . mysqli_connect_error());
